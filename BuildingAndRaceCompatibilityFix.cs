8a4d734c8892db5c47edc9a9e572457e27a5b15d
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-01 10:54:53 +0200

    added auto patching of spam logs

--- BuildingAndRaceCompatibilityFix.cs
+++ BuildingAndRaceCompatibilityFix.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using ai.behaviours;
 using HarmonyLib;
 using NeoModLoader.api;
@@ -8,6 +11,7 @@ using NeoModLoader.api;
 namespace BuildingAndRaceCompatibilityFix {
   [SuppressMessage("ReSharper", "InconsistentNaming")]
   public class BuildingAndRaceCompatibilityFix : BasicMod<BuildingAndRaceCompatibilityFix> {
+    private static byte _initCounter = 0;
     protected override void OnModLoad() {
       LogInfo("BuildingAndRaceCompatibilityFix loading...");
       try {
@@ -17,10 +21,31 @@ namespace BuildingAndRaceCompatibilityFix {
           finalizer: new HarmonyMethod(typeof(BuildingAndRaceCompatibilityFix), nameof(CityBehBuild_canUseBuildAsset_Finalizer))
         );
       } catch (Exception e) {
-        LogError("BuildingAndRaceCompatibilityFix failed to load!");
+        LogError("BuildingAndRaceCompatibilityFix failed to load fully!");
         LogError(e.ToString());
       }
       LogInfo("BuildingAndRaceCompatibilityFix loaded!");
+    }
+
+    private void Update() {
+      if (_initCounter == 4) {
+        foreach (Type type in NeoModLoader.WorldBoxMod.LoadedMods.Select(mod => mod.GetType()).Select(modType => modType.Assembly).SelectMany(modAssembly => modAssembly.GetTypes())) {
+          LogInfo($"Checking {type.FullName} for aggressive getBuildingAsset() log patches...");
+          foreach (MethodInfo method in type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).Where(method => method.Name == "getBuildingAsset_Prefix")) {
+            LogInfo($"Found {type.FullName}.{method.Name}!");
+            try {
+              Harmony harmony = new Harmony($"key.worldbox.{type.Name}.auto_patch");
+              harmony.Patch(
+                method,
+                transpiler: new HarmonyMethod(typeof(BuildingAndRaceCompatibilityFix), nameof(RemoveLoggingFromMethod))
+              );
+            } catch (Exception e) {
+              LogError($"Failed to patch {type.FullName}.{method.Name}!");
+              LogError(e.ToString());
+            }
+          }
+        }
+      } else if (_initCounter < 4) _initCounter++;
     }
 
     private static Exception CityBehBuild_canUseBuildAsset_Finalizer(Exception __exception, ref bool __result) {
@@ -29,6 +54,16 @@ namespace BuildingAndRaceCompatibilityFix {
         return null;
       }
       return __exception;
+    }
+    
+    private static IEnumerable<CodeInstruction> RemoveLoggingFromMethod(IEnumerable<CodeInstruction> instructions) {
+      foreach (CodeInstruction instruction in instructions) {
+        if (instruction.opcode == OpCodes.Call && instruction.operand is MethodInfo methodInfo && methodInfo.Name == "Log") {
+          yield return new CodeInstruction(OpCodes.Pop);
+        } else {
+          yield return instruction;
+        }
+      }
     }
   }
 }
