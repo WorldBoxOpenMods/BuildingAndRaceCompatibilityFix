5fb76c9a0dce600f0b6f8a1dabde441dc472f193
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-01 10:34:16 +0200

    gave BuildingAndRaceCompatibilityFix a failsafe against using outdated NML versions with it

---
+++ OutdatedNml.cs
@@ -0,0 +1,23 @@
+using NeoModLoader.api;
+using UnityEngine;
+
+namespace BuildingAndRaceCompatibilityFix {
+  public class OutdatedNml : MonoBehaviour, IMod {
+    private ModDeclare _modDeclare;
+    private GameObject _gameObject;
+    public ModDeclare GetDeclaration() {
+      return _modDeclare;
+    }
+    public GameObject GetGameObject() {
+      return _gameObject;
+    }
+    public string GetUrl() {
+      return "https://github.com/WorldBoxOpenMods/BuildingAndRaceCompatibilityFix";
+    }
+    public void OnLoad(ModDeclare pModDecl, GameObject pGameObject) {
+      _modDeclare = pModDecl;
+      _gameObject = pGameObject;
+      Debug.LogError("You're using an outdated NML version that doesn't support precompiled mods. Please get version 1.0.9 of NeoModLoader or later.");
+    }
+  }
+}
