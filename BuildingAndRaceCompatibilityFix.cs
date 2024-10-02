58d7cbebbed7ddfba9887f3adef4490498567a5d
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-02 10:48:37 +0200

    my short version tripped up the compiler

--- BuildingAndRaceCompatibilityFix.cs
+++ BuildingAndRaceCompatibilityFix.cs
@@ -79,8 +79,9 @@ namespace BuildingAndRaceCompatibilityFix {
     }
 
     private static bool HasModEntryAttribute(MonoBehaviour component) {
-      #pragma warning disable once CS0618 // Type or member is obsolete
+      #pragma warning disable CS0618 // Type or member is obsolete
       return Attribute.GetCustomAttribute(component.GetType(), typeof(ModEntry)) != null;
+      #pragma warning restore CS0618 // Type or member is obsolete
     }
 
     private static bool IsBaseUnityPluginDerivative(MonoBehaviour component) {
